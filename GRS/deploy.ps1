d74b917907c3e9b801046a2aa66e8622af2c787b
Thiago Di Giorgio <thiago.digiorgio@version1.com>
2019-05-17 17:46:41 +0100

    j

--- GRS/deploy.ps1
+++ GRS/deploy.ps1
@@ -2,6 +2,9 @@ Login-AzAccount
 
 Select-AzSubscription -Subscription "UK D&C Solutions"
 
+Get-AzMarketplaceTerms -Publisher fortinet -Product fortinet_fortigate-vm_v5 -name fortinet_fg-vm_payg | Set-AzMarketplaceTerms -Accept
+
+
 Test-AzDeployment  -Location "uksouth" -TemplateFile ".\GRS\master-template.json" -Verbose 
 
 New-AzDeployment  -Location "uksouth" -TemplateFile ".\GRS\master-template.json" -Verbose 
